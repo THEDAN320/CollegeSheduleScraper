341814bda2c7389063caf7e94e4c020c6ce28a9c
THEDAN320 <danilmironenko897@gmail.com>
2024-09-17 17:11:52 +0700

    develop

--- main.py
+++ main.py
@@ -5,17 +5,9 @@ from routers import router
 
 app = FastAPI(title="College Scraper")
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://localhost:5555",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
